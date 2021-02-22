7bd40370ddbcbde9b38c2ff1722fba31fa92f036
yangmingtian <18516398527@163.com>
2021-02-22 11:23:25 +0800

    dd

--- src/main/java/com/example/demo/algorithm/Bst.java
+++ src/main/java/com/example/demo/algorithm/Bst.java
@@ -1,126 +1,60 @@
 package com.example.demo.algorithm;
 
-import javax.swing.*;
-import java.util.concurrent.atomic.AtomicInteger;
+import sun.plugin.cache.OldCacheEntry;
+// 优先级队列是基于二叉堆实现的，主要操作是插入和删除。
+// 插入是先插到最后，然后上浮到正确位置；
+// 删除是把第一个元素 pq[1]（最值）调换到最后再删除，然后把新的 pq[1] 下沉到正确位置
+// 堆
+public class BST {
 
-/**
- * Created by @author ymtNSN on 2021/2/5
- */
-public class Bst {
+    int count;
 
-    /**
-     * 判断 BST 的合法性
-     *
-     * @param root
-     * @return
-     */
-    private boolean isValidBST(TreeNode root) {
-        return isValidBST(root, null, null);
+    // 父节点索引
+    int parent(int root) {
+        return root / 2;
     }
 
-
-    private boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
-        if (root == null) {
-            return true;
-        }
-        if (min != null && root.val <= min.val) {
-            return false;
-        }
-        if (max != null && root.val >= max.val) {
-            return false;
-        }
-        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    int left(int root) {
+        return root * 2;
     }
 
-    /**
-     * 在 BST 中查找⼀个数是否存在
-     */
-    private boolean isInBst(TreeNode root, int target) {
-        if (root == null) {
-            return false;
-        }
-        if (root.val == target) {
-            return true;
-        }
-        if (root.val > target) {
-            return isInBst(root.left, target);
-        } else {
-            return isInBst(root.right, target);
+    int right(int root) {
+        return root * 2 + 1;
+    }
+
+    // 上浮只需要跟父节点比较就好
+    private void swim(int k){
+        while (k > 1 && less(parent(k),k)){
+            exch(parent(k),k);
+            k = parent(k);
         }
     }
 
-    /**
-     * 在 BST 中删除⼀个数
-     */
-    public TreeNode deleteNode(TreeNode root, int target) {
-        if (root == null) {
-            return null;
-        }
-        if (root.val == target) {
-            if (root.left == null) {
-                return root.right;
+    // 下沉需要跟两个节点比较
+    private void sink(int k){
+        while (left(k) <= count){
+            int max = left(k);
+            if (right(k) <= count && less(max,right(k))){
+                max = right(k);
             }
-            if (root.right == null) {
-                return root.left;
+            if (less(max,k)){
+                break;
             }
-            TreeNode minNode = getMin(root.right);
-            root.val = minNode.val;
-            root.right = deleteNode(root.right, minNode.val);
-        } else if (root.val > target) {
-            root.left = deleteNode(root.left, target);
-        } else {
-            root.right = deleteNode(root.right, target);
-        }
-        return root;
-    }
-
-    private TreeNode getMin(TreeNode node) {
-        while (node != null) {
-            node = node.left;
-        }
-        return node;
-    }
-
-
-    class TreeNode {
-        private int val;
-        private TreeNode left;
-        private TreeNode right;
-
-        public TreeNode(int val) {
-            this(val, null, null);
-        }
-
-        public TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
+            exch(k, max);
+            k = max;
         }
     }
-    private final AtomicInteger ctl = new AtomicInteger(ctlOf(RUNNING, 0));
-    private static final int COUNT_BITS = Integer.SIZE - 3;
-    private static final int CAPACITY   = (1 << COUNT_BITS) - 1;
 
-    // runState is stored in the high-order bits
-    private static final int RUNNING    = -1 << COUNT_BITS;
-    private static final int SHUTDOWN   =  0 << COUNT_BITS;
-    private static final int STOP       =  1 << COUNT_BITS;
-    private static final int TIDYING    =  2 << COUNT_BITS;
-    private static final int TERMINATED =  3 << COUNT_BITS;
+    private void exch(int parent, int k) {
 
-    // Packing and unpacking ctl
-    private static int runStateOf(int c)     { return c & ~CAPACITY; }
-    private static int workerCountOf(int c)  { return c & CAPACITY; }
-    private static int ctlOf(int rs, int wc) { return rs | wc; }
 
-    public static void main(String[] args) {
-        System.out.println(Integer.toBinaryString(-1));
-        System.out.println(Integer.toBinaryString(COUNT_BITS));
-        System.out.println(Integer.toBinaryString(RUNNING));
-        System.out.println(Integer.toBinaryString(SHUTDOWN));
-        System.out.println(Integer.toBinaryString(STOP));
-        System.out.println(Integer.toBinaryString(STOP-1));
-        System.out.println(Integer.toBinaryString(TIDYING));
-        System.out.println(Integer.toBinaryString(TERMINATED));
+    }
+
+    private boolean less(int parent, int k) {
+        return false;
+    }
+
+    class Node {
+
     }
 }
